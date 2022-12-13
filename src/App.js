be52c92642fc8a0908643ec905a851b219e66998
Allan Ikeda <allanikeda@outlook.com>
2022-12-13 15:30:06 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Search from './components/Search';
+import Album from './components/Album';
+import Favorite from './components/Favorites';
+import Profile from './components/Profile';
+import ProfileEdit from './components/ProfileEdit';
+import NotFound from './components/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <Route path="/search" component={ Search } />
+        <Route path="/album" component={ Album } />
+        <Route path="/favorites" component={ Favorite } />
+        <Route exact path="/profile" component={ Profile } />
+        <Route exact path="/profile/edit" component={ ProfileEdit } />
+        <Route exact path="/" component={ Login } />
+        <Route path="*" component={ NotFound } />
+      </BrowserRouter>
+    );
   }
 }
-
-// iniciando
 
 export default App;
