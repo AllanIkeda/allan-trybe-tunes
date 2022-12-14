bad1c778d9af6b3c9bc7c3173786eb3ec89ecdd2
Allan Ikeda <allanikeda@outlook.com>
2022-12-14 15:14:16 -0300

    requisito 6 no caminho

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from './Loading';
 
 export default class Search extends Component {
   state = {
     disable: true,
-    // isLoading: false,
+    albuns: [],
+    valueSearch: '',
+    isLoading: false,
+    result: false,
+    // artist: '',
   };
 
   validationBtn = ({ target: { value } }) => {
@@ -12,16 +18,41 @@ export default class Search extends Component {
     const compara = value.length >= TAM_MAX;
     this.setState({
       disable: !compara,
+      valueSearch: value,
     });
   };
 
+  handleClick = async () => {
+    const { valueSearch } = this.state;
+    this.setState({
+      isLoading: true,
+      result: true,
+    });
+    const albuns = await searchAlbumsAPI(valueSearch);
+    // const artist = albuns.find((artista) => artista).artistName;
+    // console.log(albuns);
+    // console.log(albuns.find((artista) => artista).artistName);
+    this.setState({
+      albuns,
+      valueSearch: '',
+      isLoading: false,
+      // artist,
+    });
+    if (albuns.length === 0) {
+      this.setState({ result: false });
+    }
+  };
+
   render() {
-    const { disable } = this.state;
+    const { disable, valueSearch, isLoading, result, artist, albuns } = this.state;
     return (
       <div data-testid="page-search">
+        { !result && <p>Nenhum álbum foi encontrado</p> }
+        { isLoading && <Loading />}
         <Header />
         <form>
           <input
+            value={ valueSearch }
             type="text"
             data-testid="search-artist-input"
             onChange={ this.validationBtn }
@@ -30,12 +61,22 @@ export default class Search extends Component {
             type="button"
             data-testid="search-artist-button"
             disabled={ disable }
-            // onClick={ this.validationBtn }
+            onClick={ this.handleClick }
           >
             Pesquisar
           </button>
         </form>
-        Search
+        {result
+          ? (
+            <h3>
+              {`Resultado de álbuns de: ${artist}`}
+            </h3>
+          ) : null}
+        { albuns.map((album) => (
+          <div key={ album.collectionId }>
+            <img src={ album.artworkUrl100 } alt="foto-album" />
+          </div>
+        ))}
       </div>
     );
   }
