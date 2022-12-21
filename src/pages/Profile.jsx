07337ba4a39f2705833cbe62384482a242e8d412
Allan Ikeda <allanikeda@outlook.com>
2022-12-21 16:11:45 -0400

    WIP - requisito 13 recuperando os dados do User

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 export default class Profile extends Component {
+  state = {
+    name: '',
+    email: '',
+    image: '',
+    description: '',
+    isLoading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ isLoading: true });
+    const { name, email, image, description } = await getUser();
+    this.setState({
+      name,
+      email,
+      image,
+      description,
+      isLoading: false,
+    });
+  }
+
   render() {
+    const { name, email, image, description, isLoading } = this.state;
     return (
-      <>
+      <div data-testid="page-profile">
+        { isLoading && <Loading /> }
         <Header />
-        <div data-testid="page-profile">Profile</div>
-      </>
+        <section>
+          <img src={ image } data-testid="profile-image" alt="imagem-de-perfil" />
+          <h3>Nome</h3>
+          <p>{ name }</p>
+          <h3>Email</h3>
+          <p>{ email }</p>
+          <h3>Descrição</h3>
+          <p>{ description }</p>
+        </section>
+      </div>
     );
   }
 }
